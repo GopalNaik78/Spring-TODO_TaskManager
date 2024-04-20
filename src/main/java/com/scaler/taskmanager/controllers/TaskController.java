c881272214cc23ac5b8c2a1d9b2b84e0570d4ad0
GopalNail <ajaynaik8971@gmail.com>
2024-04-20 13:01:12 +0530

    edit task

--- src/main/java/com/scaler/taskmanager/controllers/TaskController.java
+++ src/main/java/com/scaler/taskmanager/controllers/TaskController.java
@@ -1,11 +1,14 @@
 package com.scaler.taskmanager.controllers;
 
 import com.scaler.taskmanager.dtos.CreateTaskDTO;
+import com.scaler.taskmanager.dtos.ErrorResponseDTO;
+import com.scaler.taskmanager.dtos.UpdateTaskDTO;
 import com.scaler.taskmanager.entities.TaskEntity;
 import com.scaler.taskmanager.services.TaskService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -34,8 +37,26 @@ public class TaskController {
     }
 
     @PostMapping("")
-    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body){
+    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body) throws ParseException {
         var task=taskService.addTask(body.getTitle(),body.getDescription(),body.getDeadline());
         return ResponseEntity.ok(task);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<TaskEntity> updateTask(@PathVariable int id, @RequestBody UpdateTaskDTO body) throws ParseException {
+        var task = taskService.updateTask(id,body.getDescription(),body.getDeadline(),body.getCompleted());
+
+        if (task == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(task);
+    }
+
+
+    @ExceptionHandler(ParseException.class)
+    public ResponseEntity<ErrorResponseDTO> handleErrors(Exception e){
+        if(e instanceof ParseException){
+            return ResponseEntity.badRequest().body(new ErrorResponseDTO("Invalid date Format"));
+        }
+        return ResponseEntity.internalServerError().body(new ErrorResponseDTO("Internal Server Error"));
     }
 }
--- src/main/java/com/scaler/taskmanager/services/TaskService.java
+++ src/main/java/com/scaler/taskmanager/services/TaskService.java
@@ -3,19 +3,22 @@ package com.scaler.taskmanager.services;
 import com.scaler.taskmanager.entities.TaskEntity;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 @Service
 public class TaskService {
     private ArrayList<TaskEntity> tasks= new ArrayList<TaskEntity>();
     private int taskId=1;
+    private final SimpleDateFormat deadlineFormat = new SimpleDateFormat("yyyy-MM-dd");
     
-    public TaskEntity addTask(String title, String description, String deadline){
+    public TaskEntity addTask(String title, String description, String deadline) throws ParseException {
         TaskEntity task=new TaskEntity();
         task.setId(taskId);
         task.setTitle(title);
         task.setDescription(description);
-        //task.setDeadline(new Date(deadline)); //TODO: validate date format YYYY-MM-DD
+        task.setDeadline(deadlineFormat.parse(deadline)); //TODO: validate date format YYYY-MM-DD
         task.setCompleted(false);
         tasks.add(task);
         taskId++;
@@ -34,5 +37,19 @@ public class TaskService {
             }
         }
         return null;
+    }
+
+    public TaskEntity updateTask(int id, String description, String deadline, Boolean completed ) throws ParseException {
+        TaskEntity task = getTaskById(id);
+        if(task == null)
+            return null;
+
+        if(description != null)
+            task.setDescription(description);
+        if(deadline != null)
+            task.setDeadline(deadlineFormat.parse(deadline));
+        if(completed != null)
+            task.setCompleted(completed);
+        return task;
     }
 }
