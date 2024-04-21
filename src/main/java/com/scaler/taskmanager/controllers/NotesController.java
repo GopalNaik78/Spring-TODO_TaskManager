b495577c8808db31ca6d092a4e0b55e9cbb4a0d9
GopalNail <ajaynaik8971@gmail.com>
2024-04-21 14:25:06 +0530

    add notes part2

--- src/main/java/com/scaler/taskmanager/controllers/NotesController.java
+++ src/main/java/com/scaler/taskmanager/controllers/NotesController.java
@@ -1,10 +1,13 @@
 package com.scaler.taskmanager.controllers;
 
+import com.scaler.taskmanager.dtos.CreateNoteDTO;
+import com.scaler.taskmanager.dtos.CreateNoteResponseDTO;
+import com.scaler.taskmanager.entities.NoteEntity;
 import com.scaler.taskmanager.services.NotesService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks/{taskId}/notes")
@@ -16,7 +19,19 @@ public class NotesController {
     }
 
     @GetMapping("")
-    public String getNotes(@PathVariable("taskId") Integer taskId){
+    public ResponseEntity<List<NoteEntity>> getNotes(@PathVariable("taskId") Integer taskId){
+        var notes = notesService.getNotesForTask(taskId);
 
+        return ResponseEntity.ok(notes);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<CreateNoteResponseDTO> addNote(
+            @PathVariable ("taskId")Integer taskId,
+            @RequestBody CreateNoteDTO body
+    ){
+        var note = notesService.addNoteForTask(taskId, body.getTitle(),body.getBody());
+
+        return ResponseEntity.ok(new CreateNoteResponseDTO(taskId,note));
     }
 }
